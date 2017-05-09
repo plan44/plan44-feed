60816aef7a0a83c33918db93c4ff0e67f2cf8514
Lukas Zeller <luz@plan44.ch>
2017-05-09 23:04:00 +0200

    pixelboard-config: p44b saved build, mixed plan44 and p44i

---
+++ pixelboard-config/p44build/tracking/pushtags.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
+git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/pixelboardd.git pixelboardd
+pushd pixelboardd
+git tag pixelboard-omega2_0.13 dfd1824b49767f4e06c3884d20bf83817c72be92
+git push origin pixelboard-omega2_0.13
+popd
+git clone --depth 1 --no-checkout --single-branch --branch ow_testing ssh://plan44@plan44.ch/home/plan44/sharedgit/mg44.git mg44
+pushd mg44
+git tag pixelboard-omega2_0.13 002b1e8408c823317afa9ed10508d3b35af24ce5
+git push origin pixelboard-omega2_0.13
+popd
+git clone --depth 1 --no-checkout --single-branch --branch ow_testing ssh://plan44@plan44.ch/home/plan44/gitrepositories/serialfwd.git serialfwd
+pushd serialfwd
+git tag pixelboard-omega2_0.13 e198c8ec40a31b6e39687125efa159950f55e3e5
+git push origin pixelboard-omega2_0.13
+popd
+git clone --depth 1 --no-checkout --single-branch --branch HEAD git@github.com:OnionIoT/omega2-ctrl.git omega2-ctrl
+pushd omega2-ctrl
+git tag pixelboard-omega2_0.13 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
+git push origin pixelboard-omega2_0.13
+popd
+popd
