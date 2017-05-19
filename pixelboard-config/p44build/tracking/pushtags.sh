74c66c8332972af977b2b413a169c89767ad2156
Lukas Zeller <luz@plan44.ch>
2017-05-19 12:26:09 +0200

    pixelboard-config: pixelboard 0.4 with working Game Of Life and better controls (e.g. pause for blocks game)

--- pixelboard-config/p44build/tracking/pushtags.sh
+++ pixelboard-config/p44build/tracking/pushtags.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
-git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/pixelboardd.git pixelboardd
-pushd pixelboardd
-git tag pixelboard-omega2_0.14 c4a29c9d00677ac11b7ee1c600cbd2a5f5d0f7c9
-git push origin pixelboard-omega2_0.14
-popd
 git clone --depth 1 --no-checkout --single-branch --branch ow_testing ssh://plan44@plan44.ch/home/plan44/sharedgit/mg44.git mg44
 pushd mg44
 git tag pixelboard-omega2_0.14 002b1e8408c823317afa9ed10508d3b35af24ce5
