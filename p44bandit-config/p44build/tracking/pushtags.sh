9e831b18173c85b17ab4a783f6a2a65ea62eb3a3
Lukas Zeller <luz@plan44.ch>
2017-10-20 16:55:37 +0200

    p44bandit-config: p44b save snapshot after first successful LEDE v17.01.4 build

--- p44bandit-config/p44build/tracking/pushtags.sh
+++ p44bandit-config/p44build/tracking/pushtags.sh
@@ -10,7 +10,12 @@ pushd serialfwd
 git tag -f P44BANDIT/0.4/p44bandit-omega2 e198c8ec40a31b6e39687125efa159950f55e3e5
 git push -f origin P44BANDIT/0.4/p44bandit-omega2
 popd
-git clone --depth 1 --no-checkout --single-branch --branch HEAD git@github.com:OnionIoT/omega2-ctrl.git omega2-ctrl
+git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/p44banditd.git p44banditd
+pushd p44banditd
+git tag -f P44BANDIT/0.4/p44bandit-omega2 554a2eb5c11d3f1b8a8a0e276a97939a9c062ff7
+git push -f origin P44BANDIT/0.4/p44bandit-omega2
+popd
+git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
 pushd omega2-ctrl
 git tag -f P44BANDIT/0.4/p44bandit-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
 git push -f origin P44BANDIT/0.4/p44bandit-omega2
