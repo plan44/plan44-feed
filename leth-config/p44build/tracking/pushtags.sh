4b38ceecb8ac41bcb9a315b509d1d7ae81be4f54
Lukas Zeller <luz@plan44.ch>
2018-08-10 22:36:59 +0000

    LETH/0.6/leth-omega2

--- leth-config/p44build/tracking/pushtags.sh
+++ leth-config/p44build/tracking/pushtags.sh
@@ -2,7 +2,7 @@
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
 git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/lethd.git lethd
 pushd lethd
-git tag -f LETH/0.6/leth-omega2 012a626d440f258ecf24b2a1a09461e910f374cc
+git tag -f LETH/0.6/leth-omega2 ac88b2cb5982a24cf4c50931137c3a0d340a8bab
 git push -f origin LETH/0.6/leth-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
