40e1ac736909037388c93a0eed497049b52b4d84
Lukas Zeller <luz@plan44.ch>
2018-08-10 22:04:57 +0200

    leth-config: first successful openwrt 18.06.0 build with working ledchain, pwm and mcp3002 via soft spi

--- leth-config/p44build/tracking/pushtags.sh
+++ leth-config/p44build/tracking/pushtags.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
-git clone --depth 1 --no-checkout --single-branch --branch ow_testing ssh://plan44@plan44.ch/home/plan44/sharedgit/mg44.git mg44
-pushd mg44
-git tag -f LETH/0.5/leth-omega2 6df3c33a2d0911f67fe1cf2313aa8cd79a74546e
-git push -f origin LETH/0.5/leth-omega2
-popd
 git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/lethd.git lethd
 pushd lethd
-git tag -f LETH/0.5/leth-omega2 012a626d440f258ecf24b2a1a09461e910f374cc
-git push -f origin LETH/0.5/leth-omega2
+git tag -f LETH/0.6/leth-omega2 012a626d440f258ecf24b2a1a09461e910f374cc
+git push -f origin LETH/0.6/leth-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
 pushd omega2-ctrl
-git tag -f LETH/0.5/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
-git push -f origin LETH/0.5/leth-omega2
+git tag -f LETH/0.6/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
+git push -f origin LETH/0.6/leth-omega2
 popd
 popd
