66c31bb36cb0fda37ebb7b81f81d16b09a2a3d74
Lukas Zeller <luz@plan44.ch>
2018-08-19 18:54:57 +0000

    LETH/1.0/leth-omega2

--- leth-config/p44build/tracking/pushtags.sh
+++ leth-config/p44build/tracking/pushtags.sh
@@ -2,17 +2,17 @@
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
 git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/mg44.git mg44
 pushd mg44
-git tag -f LETH/0.9/leth-omega2 4edf2903b6c77c92c84d3a7bbfd3d19530df6cab
-git push -f origin LETH/0.9/leth-omega2
+git tag -f LETH/1.0/leth-omega2 4edf2903b6c77c92c84d3a7bbfd3d19530df6cab
+git push -f origin LETH/1.0/leth-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/lethd.git lethd
 pushd lethd
-git tag -f LETH/0.9/leth-omega2 24e1c74f5e80d277759de05da498d6124b5856da
-git push -f origin LETH/0.9/leth-omega2
+git tag -f LETH/1.0/leth-omega2 28e6d594fd44d098a037a2800f39dc9fc5a3dcae
+git push -f origin LETH/1.0/leth-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
 pushd omega2-ctrl
-git tag -f LETH/0.9/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
-git push -f origin LETH/0.9/leth-omega2
+git tag -f LETH/1.0/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
+git push -f origin LETH/1.0/leth-omega2
 popd
 popd
