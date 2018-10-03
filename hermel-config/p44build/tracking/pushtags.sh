0f97c0a2959638fe882a225143d1d3b452ba7561
Lukas Zeller <luz@plan44.ch>
2018-10-03 20:39:41 +0200

    HERMEL/0.1/leth-omega2

---
+++ hermel-config/p44build/tracking/pushtags.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
+git clone --depth 1 --no-checkout --single-branch --branch hermeld ssh://plan44@plan44.ch/home/plan44/sharedgit/lethd.git hermeld
+pushd hermeld
+git tag -f HERMEL/0.1/leth-omega2 3633cb5ad2ae73bf9818d926a0d85dd4c84318f5
+git push -f origin HERMEL/0.1/leth-omega2
+popd
+git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
+pushd omega2-ctrl
+git tag -f HERMEL/0.1/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
+git push -f origin HERMEL/0.1/leth-omega2
+popd
+popd
