a6d2b9784bacc21bc30f034258d8f8cf2ac4b8b0
Lukas Zeller <luz@plan44.ch>
2018-12-03 18:22:57 +0000

    hermel-config: Update for new hermeld 1.95 (API + script changes), reduced WebUI to sending a message (for now).

--- hermel-config/p44build/tracking/pushtags.sh
+++ hermel-config/p44build/tracking/pushtags.sh
@@ -2,12 +2,12 @@
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
 git clone --depth 1 --no-checkout --single-branch --branch hermeld ssh://plan44@plan44.ch/home/plan44/sharedgit/lethd.git hermeld
 pushd hermeld
-git tag -f HERMEL/1.94/leth-omega2 320d7c7d0dd81650936cf1822d62d32718e5e891
-git push -f origin HERMEL/1.94/leth-omega2
+git tag -f HERMEL/1.95/leth-omega2 8ebdd44d245782c32aa713c7ab26a998f9b9fd07
+git push -f origin HERMEL/1.95/leth-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
 pushd omega2-ctrl
-git tag -f HERMEL/1.94/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
-git push -f origin HERMEL/1.94/leth-omega2
+git tag -f HERMEL/1.95/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
+git push -f origin HERMEL/1.95/leth-omega2
 popd
 popd
