7f0a0f701f1cd87cfafe3d27b3bc98fd80148c47
Lukas Zeller <luz@plan44.ch>
2019-02-21 22:02:11 +0100

    p44bandit-config: v18.06.2 build with fbtft and ssd1306 driver working as /dev/fb0 and linux console!
    Note: needs manual insert of chip driver (fb_ssd1306) and parametrizing through fbtft_device to instantiate frame buffer device!

--- p44bandit-config/p44build/tracking/pushtags.sh
+++ p44bandit-config/p44build/tracking/pushtags.sh
@@ -2,22 +2,22 @@
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
 git clone --depth 1 --no-checkout --single-branch --branch ow_testing ssh://plan44@plan44.ch/home/plan44/sharedgit/mg44.git mg44
 pushd mg44
-git tag -f P44BANDIT/0.4/p44bandit-omega2 4edf2903b6c77c92c84d3a7bbfd3d19530df6cab
-git push -f origin P44BANDIT/0.4/p44bandit-omega2
+git tag -f P44BANDIT/0.6/p44bandit-omega2 87d807c0ba3b782f92dbc8bb6a0bbacaaf2c6ea0
+git push -f origin P44BANDIT/0.6/p44bandit-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch ow_testing ssh://plan44@plan44.ch/home/plan44/gitrepositories/serialfwd.git serialfwd
 pushd serialfwd
-git tag -f P44BANDIT/0.4/p44bandit-omega2 e198c8ec40a31b6e39687125efa159950f55e3e5
-git push -f origin P44BANDIT/0.4/p44bandit-omega2
+git tag -f P44BANDIT/0.6/p44bandit-omega2 e198c8ec40a31b6e39687125efa159950f55e3e5
+git push -f origin P44BANDIT/0.6/p44bandit-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/p44banditd.git p44banditd
 pushd p44banditd
-git tag -f P44BANDIT/0.4/p44bandit-omega2 554a2eb5c11d3f1b8a8a0e276a97939a9c062ff7
-git push -f origin P44BANDIT/0.4/p44bandit-omega2
+git tag -f P44BANDIT/0.6/p44bandit-omega2 554a2eb5c11d3f1b8a8a0e276a97939a9c062ff7
+git push -f origin P44BANDIT/0.6/p44bandit-omega2
 popd
 git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
 pushd omega2-ctrl
-git tag -f P44BANDIT/0.4/p44bandit-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
-git push -f origin P44BANDIT/0.4/p44bandit-omega2
+git tag -f P44BANDIT/0.6/p44bandit-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
+git push -f origin P44BANDIT/0.6/p44bandit-omega2
 popd
 popd
