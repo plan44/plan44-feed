aef31cce152fcf812201ef58bbb987a43b466a9c
Lukas Zeller <luz@plan44.ch>
2019-03-06 10:29:24 +0000

    leth-config: update to openwrt v18.06.2, add patches for offering display drivers in menuconfig, HW SPI working for displays, and safer prevention of accidental dhcp server

--- leth-config/p44build/tracking/pushtags.sh
+++ leth-config/p44build/tracking/pushtags.sh
@@ -2,12 +2,13 @@
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
 git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/lethd.git lethd
 pushd lethd
-git tag -f LETH/1.9.1/leth-omega2 ea65d49d9a9bd1bf07cee9f6c5eb90d18958c095
-git push -f origin LETH/1.9.1/leth-omega2
+git tag -f LETH/1.9.2/leth-omega2 ea65d49d9a9bd1bf07cee9f6c5eb90d18958c095
+git push -f origin LETH/1.9.2/leth-omega2
 popd
-git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
+# onion/omega2-ctrl: WARNING: There is no branch/tag currently at fa0425d861abbcc34bacd6edc3c7b531ad32c4d9. We need to checkout whole repo
+git clone --no-checkout https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
 pushd omega2-ctrl
-git tag -f LETH/1.9.1/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
-git push -f origin LETH/1.9.1/leth-omega2
+git tag -f LETH/1.9.2/leth-omega2 fa0425d861abbcc34bacd6edc3c7b531ad32c4d9
+git push -f origin LETH/1.9.2/leth-omega2
 popd
 popd
