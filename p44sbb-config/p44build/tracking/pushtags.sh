e66105c3d6ae9b0c48504bcedf0f9d828388d226
Lukas Zeller <luz@plan44.ch>
2020-09-07 14:02:39 +0200

    P44SBB/0.7/p44sbb-omega2
    With MixWerk Ungenauhr and J.L config

--- p44sbb-config/p44build/tracking/pushtags.sh
+++ p44sbb-config/p44build/tracking/pushtags.sh
@@ -5,9 +5,9 @@ pushd omega2-ctrl
 git tag -f P44SBB/0.7/p44sbb-omega2 e7152ba869b1af10e3c4d22938100962eb1d9b0e
 git push -f origin P44SBB/0.7/p44sbb-omega2
 popd
-git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/p44sbbd.git p44sbbd
-pushd p44sbbd
-git tag -f P44SBB/0.7/p44sbb-omega2 b1f66a5814d7bbc8a75f80acc354d95298523082
+git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/p44featured.git p44featured
+pushd p44featured
+git tag -f P44SBB/0.7/p44sbb-omega2 1beee8f976d9d2873978bd1702a8c96f77aa2d9f
 git push -f origin P44SBB/0.7/p44sbb-omega2
 popd
 popd
