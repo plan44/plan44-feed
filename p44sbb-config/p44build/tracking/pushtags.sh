41c8e9431c98a02417d82c29e0b7a879c37be7c1
Lukas Zeller <luz@plan44.ch>
2021-08-31 19:38:29 +0200

    P44SBB/0.8/p44sbb-omega2 - works ok on splitflapcontroller@mixwerk

--- p44sbb-config/p44build/tracking/pushtags.sh
+++ p44sbb-config/p44build/tracking/pushtags.sh
@@ -2,12 +2,7 @@
 pushd /tmp; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
 git clone --depth 1 --no-checkout --single-branch --branch HEAD https://github.com/OnionIoT/omega2-ctrl.git omega2-ctrl
 pushd omega2-ctrl
-git tag -f P44SBB/0.7/p44sbb-omega2 e7152ba869b1af10e3c4d22938100962eb1d9b0e
-git push -f origin P44SBB/0.7/p44sbb-omega2
-popd
-git clone --depth 1 --no-checkout --single-branch --branch master https://github.com/plan44/p44featured.git p44featured
-pushd p44featured
-git tag -f P44SBB/0.7/p44sbb-omega2 1beee8f976d9d2873978bd1702a8c96f77aa2d9f
-git push -f origin P44SBB/0.7/p44sbb-omega2
+git tag -f P44SBB/0.8/p44sbb-omega2 e7152ba869b1af10e3c4d22938100962eb1d9b0e
+git push -f origin P44SBB/0.8/p44sbb-omega2
 popd
 popd
