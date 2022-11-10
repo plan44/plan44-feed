28451dfebe8c1e70badeb42ba05f2f82114834c3
Lukas Zeller <luz@plan44.ch>
2022-11-10 14:36:22 +0100

    KKS-DCM/1.1.2/kksdcm-omega2 - SPI CS active high

--- kksdcm-config/p44build/tracking/pushtags.sh
+++ kksdcm-config/p44build/tracking/pushtags.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd /tmp >/dev/null; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
 # serialfwd-1.0: cannot push to http URL https://github.com/plan44/serialfwd.git -> cannot tag original
-# kksdcmd-1.1.1: cannot push to http URL https://github.com/plan44/kksdcmd.git -> cannot tag original
+# kksdcmd-1.1.2: cannot push to http URL https://github.com/plan44/kksdcmd.git -> cannot tag original
 # omega2-ctrl-0.2: cannot push to http URL https://github.com/OnionIoT/omega2-ctrl.git -> cannot tag original
 popd >/dev/null
