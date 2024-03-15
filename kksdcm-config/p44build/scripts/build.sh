e3f329a852959e13d356399dcc6e9839204b3c18
Lukas Zeller <luz@plan44.ch>
2024-03-15 15:00:08 +0100

    kksdcmd, kksdcm-config: moved to separate feed (astrolsts)

--- kksdcm-config/p44build/scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# clean the PATH from dumb utils like little snitch that force-place entries with spaces in there
-export PATH=$(echo $PATH | sed -E "s/(:[^: ]* [^:]*):/:/")
-
-gmake $@
--- kksdcm-config/p44build/tracking/pushtags.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pushd /tmp >/dev/null; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
-# kksdcmd-1.2.1: cannot push to http URL https://github.com/plan44/kksdcmd.git -> cannot tag original
-# omega2-ctrl-0.3: cannot push to http URL https://github.com/OnionIoT/omega2-ctrl.git -> cannot tag original
-popd >/dev/null
