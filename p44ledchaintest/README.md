f5f15219ee8220fc3d5033598efadfcbaf988977
Lukas Zeller <luz@plan44.ch>
2024-11-28 22:43:17 +0100

    p44ledchaintest: add support for 4-channel LEDs (e.g. SK6812) and 16-bit colors (e.g. WS2816)

--- p44ledchaintest/README.md
+++ p44ledchaintest/README.md
@@ -3,22 +3,22 @@ p44ledchaintest
 
 This is a small utility indended for testing the p44-ledchain smart LED chain kernel driver for MT7688 SoC (and possibly other smart LED chain drivers using the same interface).
 
-(c) 2017-2021 by luz@plan44.ch
+(c) 2017-2024 by luz@plan44.ch
 
 ## Usage
 
 To get help, just type
 
     p44ledchaintest -h
-    
+
 The simplest way to use the utility is just providing one (or multiple) led devices. In this mode, up to 720 LEDs will be set to bright red:
 
     p44ledchaintest /dev/ledchain0
-    
+
 **Note**: if the p44-ledchain driver is in *variable led type mode*, the data stream always needs to contain a header specifying the led type parameters. See p44-ledchain README for details. This header can be sent with *p44ledchaintest* using the `-H` option:
 
     p44ledchaintest -H 0203000000 /dev/ledchain0
-    
+
 (0203000000 is the header for WS2813 GRB chains)
 For the following examples, I do not show the -H option, but if p44-ledchain modules is initialized in *variable led type mode*, it needs to be included.
 
@@ -37,3 +37,15 @@ So to have a light blue dot wandering over a pink background of the first 50 led
     p44ledchaintest -n 50 -r 0 -i 25 -c 0055FF -b 330033 -S /dev/ledchain0
 
 To see some statistics about timing use the `-v` option.
+
+### 4-channel RGBW LEDs (such as SK6812)
+
+Use `-4` for 4-channel LEDs. With these, the color options (`-c`, `-b`, `-s`) take a fourth byte representing the white channel.
+
+### 16bit resolution LEDs (such as WS2816)
+
+Use `-2` for LED which have 16 bit resolution, i.e. 2 bytes per color component.
+
+The color options (`-c`, `-b`, `-s`) still remain 8bit, but you can use the `-g` option to convert the 8-bit color value into a 16-bit PWM value with better perceived brightness resolution at low levels.
+
+Without `-g`, the 8-bit color value will just be multiplied by 256 for 16-bit LEDs.
--- p44ledchaintest/src/main.c
+++ p44ledchaintest/src/main.c
@@ -30,12 +30,15 @@ static void usage(const char *name)
   fprintf(stderr, "usage:\n");
   fprintf(stderr, "  %s [options] ledchaindevice [ledchaindevice, ...]\n", name);
   fprintf(stderr, "    -n numleds : number of LEDs per chain (default: %d)\n", DEFAULT_NUMLEDS);
+  fprintf(stderr, "    -2 : LED needs 2 bytes per channel (default=1)\n");
+  fprintf(stderr, "    -4 : LED has 4 channles (default=3)\n");
+  fprintf(stderr, "    -g : use gamma correction for output\n");
   fprintf(stderr, "    -i interval[ms] : update interval (default: %d)\n", DEFAULT_UPDATEINTERVAL_MS);
   fprintf(stderr, "    -r repeats : how many repeated updates, 0=continuously, (default: %d)\n", DEFAULT_NUMREPEATS);
   fprintf(stderr, "    -e inc : effect increment, (default: %d)\n", DEFAULT_EFFECTINC);
-  fprintf(stderr, "    -c rrggbb : hex color (default: %s)\n", DEFAULT_FGCOLOR);
-  fprintf(stderr, "    -b rrggbb : alternate (background) hex color (default: %s)\n", DEFAULT_BGCOLOR);
-  fprintf(stderr, "    -s rrggbb : color increment added at each step (default: %s)\n", DEFAULT_COLORSTEP);
+  fprintf(stderr, "    -c rrggbb[ww] : hex color (default: %s)\n", DEFAULT_FGCOLOR);
+  fprintf(stderr, "    -b rrggbb[ww] : alternate (background) hex color (default: %s)\n", DEFAULT_BGCOLOR);
+  fprintf(stderr, "    -s rrggbb[ww] : color increment added at each step (default: %s)\n", DEFAULT_COLORSTEP);
   fprintf(stderr, "    -H ooccttttrr : send header data (needed for ledchain in variable led type mode)\n");
   fprintf(stderr, "       (for p44-ledchain >=v6: oo=layout, cc=chip, tttt=tmaxpassive, rr=maxretries)\n");
   fprintf(stderr, "    -F : fill up / empty led chain with foreground color\n");
@@ -65,16 +68,43 @@ static long long now()
 }
 
 
+static uint16_t pwmtable[256] = {
+      0,    19,    39,    59,    79,   100,   121,   142,   163,   185,   208,   230,   253,   277,   300,   324,
+    349,   374,   399,   425,   451,   477,   504,   531,   559,   587,   616,   645,   674,   704,   735,   766,
+    797,   829,   862,   894,   928,   962,   996,  1032,  1067,  1103,  1140,  1178,  1216,  1254,  1293,  1333,
+   1373,  1414,  1456,  1498,  1542,  1585,  1630,  1675,  1721,  1767,  1814,  1862,  1911,  1961,  2011,  2062,
+   2114,  2167,  2220,  2275,  2330,  2386,  2443,  2501,  2560,  2620,  2681,  2742,  2805,  2869,  2933,  2999,
+   3066,  3134,  3203,  3273,  3344,  3416,  3489,  3564,  3639,  3716,  3794,  3874,  3954,  4036,  4119,  4204,
+   4289,  4377,  4465,  4555,  4646,  4739,  4833,  4929,  5026,  5125,  5226,  5328,  5431,  5536,  5643,  5752,
+   5862,  5974,  6088,  6203,  6321,  6440,  6561,  6684,  6809,  6936,  7065,  7196,  7329,  7465,  7602,  7741,
+   7883,  8027,  8173,  8322,  8473,  8626,  8782,  8940,  9101,  9264,  9430,  9598,  9769,  9943, 10119, 10299,
+  10481, 10666, 10854, 11045, 11239, 11436, 11636, 11839, 12046, 12255, 12469, 12685, 12905, 13128, 13355, 13586,
+  13820, 14058, 14299, 14545, 14794, 15047, 15304, 15566, 15831, 16101, 16374, 16653, 16935, 17222, 17514, 17810,
+  18111, 18417, 18727, 19043, 19363, 19689, 20019, 20355, 20696, 21043, 21395, 21752, 22115, 22484, 22859, 23240,
+  23627, 24020, 24419, 24824, 25236, 25654, 26079, 26511, 26949, 27395, 27847, 28307, 28773, 29248, 29729, 30219,
+  30716, 31221, 31734, 32255, 32784, 33322, 33868, 34423, 34986, 35559, 36140, 36731, 37331, 37940, 38560, 39189,
+  39827, 40476, 41136, 41805, 42486, 43177, 43879, 44592, 45316, 46052, 46799, 47558, 48330, 49113, 49909, 50717,
+  51538, 52373, 53220, 54081, 54955, 55843, 56745, 57662, 58593, 59539, 60499, 61475, 62466, 63473, 64496, 65535
+};
+
+
 // globals
 // - options and params from cmdline
 int numleds = DEFAULT_NUMLEDS;
+int chanbytes = 1;
+int numchannels = 3;
+int pwmgamma = 0;
 int interval = DEFAULT_UPDATEINTERVAL_MS;
-uint8_t fgcolor[3] = { 0xFF, 0, 0};
-uint8_t bgcolor[3] = { 0, 0, 0x40};
-uint8_t colorstep[3] = { 0, 0, 0};
+uint8_t fgcolor[4] = { 0xFF, 0, 0, 0 };
+uint8_t bgcolor[4] = { 0, 0, 0x40, 0 };
+uint8_t colorstep[4] = { 0, 0, 0, 0 };
 int effectinc = DEFAULT_EFFECTINC;
 int repeats = DEFAULT_NUMREPEATS;
 int verbose = 0;
+// - run time
+uint8_t *ledbuffer = NULL;
+
+
 
 enum {
   mode_static, // static foreground fill
@@ -86,14 +116,36 @@ int mode = mode_static;
 const int maxchains = 4;
 const int maxhdrlen = 20;
 
+
+void setLed(int ledidx, uint8_t* color)
+{
+  int bidx, ch;
+  if (pwmgamma) {
+    // use PWM gamma table
+    for (ch=0; ch<numchannels; ch++) {
+      for (bidx=0; bidx<chanbytes; bidx++) {
+        ledbuffer[(ledidx*numchannels+ch)*chanbytes+bidx] = (bidx==0 ? pwmtable[color[ch]]>>8 : pwmtable[color[ch]]) & 0xFF;
+      }
+    }
+  }
+  else {
+    // put color 1:1 into MSB, LSB=0
+    for (ch=0; ch<numchannels; ch++) {
+      for (bidx=0; bidx<chanbytes; bidx++) {
+        ledbuffer[(ledidx*numchannels+ch)*chanbytes+bidx] = bidx==0 ? color[ch] : 0;
+      }
+    }
+  }
+}
+
+
 int main(int argc, char **argv)
 {
   int chainFds[maxchains];
   int numchains;
-  int loopidx, cidx, lidx, eidx;
+  int loopidx, cidx, lidx, eidx, bidx;
   struct timespec ts;
   uint8_t *rawbuffer;
-  uint8_t *ledbuffer;
   const char* headerStr = NULL;
   int hdrlen = 0;
 
@@ -114,7 +166,7 @@ int main(int argc, char **argv)
   }
 
   int c;
-  while ((c = getopt(argc, argv, "hH:n:i:e:r:c:b:s:vFS")) != -1)
+  while ((c = getopt(argc, argv, "hH:n:24gi:e:r:c:b:s:vFS")) != -1)
   {
     switch (c) {
       case 'h':
@@ -122,6 +174,15 @@ int main(int argc, char **argv)
         exit(0);
       case 'n':
         numleds = atoi(optarg);
+        break;
+      case '2':
+        chanbytes = 2;
+        break;
+      case '4':
+        numchannels = 4;
+        break;
+      case 'g':
+        pwmgamma = 1;
         break;
       case 'i':
         interval = atoi(optarg);
@@ -133,13 +194,13 @@ int main(int argc, char **argv)
         repeats = atoi(optarg);
         break;
       case 'c':
-        sscanf(optarg, "%2hhx%2hhx%2hhx", &fgcolor[0], &fgcolor[1], &fgcolor[2]);
+        sscanf(optarg, "%2hhx%2hhx%2hhx%2hhx", &fgcolor[0], &fgcolor[1], &fgcolor[2], &fgcolor[3]);
         break;
       case 'b':
-        sscanf(optarg, "%2hhx%2hhx%2hhx", &bgcolor[0], &bgcolor[1], &bgcolor[2]);
+        sscanf(optarg, "%2hhx%2hhx%2hhx%2hhx", &bgcolor[0], &bgcolor[1], &bgcolor[2], &bgcolor[3]);
         break;
       case 's':
-        sscanf(optarg, "%2hhx%2hhx%2hhx", &colorstep[0], &colorstep[1], &colorstep[2]);
+        sscanf(optarg, "%2hhx%2hhx%2hhx%2hhx", &colorstep[0], &colorstep[1], &colorstep[2], &colorstep[3]);
         break;
       case 'H':
         headerStr = optarg;
@@ -173,7 +234,7 @@ int main(int argc, char **argv)
     exit(1);
   }
   // allocate buffer
-  rawbuffer = malloc(numleds*3+maxhdrlen+1);
+  rawbuffer = malloc(numleds*numchannels*chanbytes+maxhdrlen+1);
   ledbuffer = rawbuffer;
   // maybe we have a header
   if (headerStr) {
@@ -195,23 +256,17 @@ int main(int argc, char **argv)
     switch(mode) {
       case mode_static: {
         for (lidx=0; lidx<numleds; lidx++) {
-          ledbuffer[lidx*3+0] = fgcolor[0];
-          ledbuffer[lidx*3+1] = fgcolor[1];
-          ledbuffer[lidx*3+2] = fgcolor[2];
+          setLed(lidx, fgcolor);
         }
         break;
       }
       case mode_fillup:
       {
         for (lidx=0; lidx<(eidx%numleds); lidx++) {
-          ledbuffer[lidx*3+0] = fgcolor[0];
-          ledbuffer[lidx*3+1] = fgcolor[1];
-          ledbuffer[lidx*3+2] = fgcolor[2];
+          setLed(lidx, fgcolor);
         }
         for (; lidx<numleds; lidx++) {
-          ledbuffer[lidx*3+0] = bgcolor[0];
-          ledbuffer[lidx*3+1] = bgcolor[1];
-          ledbuffer[lidx*3+2] = bgcolor[2];
+          setLed(lidx, bgcolor);
         }
         break;
       }
@@ -219,14 +274,10 @@ int main(int argc, char **argv)
       {
         for (lidx=0; lidx<numleds; lidx++) {
           if ((eidx%numleds)==lidx) {
-            ledbuffer[lidx*3+0] = fgcolor[0];
-            ledbuffer[lidx*3+1] = fgcolor[1];
-            ledbuffer[lidx*3+2] = fgcolor[2];
+            setLed(lidx, fgcolor);
           }
           else {
-            ledbuffer[lidx*3+0] = bgcolor[0];
-            ledbuffer[lidx*3+1] = bgcolor[1];
-            ledbuffer[lidx*3+2] = bgcolor[2];
+            setLed(lidx, bgcolor);
           }
         }
         break;
@@ -235,7 +286,7 @@ int main(int argc, char **argv)
     // update chains
     beforeUpdate = now();
     for (cidx = 0; cidx<numchains; cidx++) {
-      write(chainFds[cidx], rawbuffer, numleds*3+hdrlen);
+      write(chainFds[cidx], rawbuffer, numleds*numchannels*chanbytes+hdrlen);
     }
     afterUpdate = now();
     // calculate remaining wait time
@@ -265,6 +316,7 @@ int main(int argc, char **argv)
     fgcolor[0] += colorstep[0];
     fgcolor[1] += colorstep[1];
     fgcolor[2] += colorstep[2];
+    fgcolor[3] += colorstep[3];
   }
   printf("TOTAL time: %lld, average per loop: %lld [microseconds]\n", total, total/loopidx);
   // close
