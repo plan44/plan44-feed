4a961ba463970343095fc0f8846dea82f3d5f194
Lukas Zeller <luz@plan44.ch>
2024-12-05 19:51:38 +0100

    p44-xx-diy-config: add basic files

---
+++ p44-xx-diy-config/README.md
@@ -0,0 +1,5 @@
+# How to build the P44-xx-diy firmware images based on OpenWrt
+
+tbd.
+
+For now, see README.md of umbrella repo [github.com/plan44/p44-xx-open](https://github.com/plan44/p44-xx-open)
---
+++ p44-xx-diy-config/p44build/tracking/pushtags.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pushd /tmp >/dev/null; rm -rf /tmp/p44b_temp; mkdir p44b_temp; cd p44b_temp
+echo "vdcd-2.8.1.8: cannot push to http URL https://github.com/plan44/vdcd.git -> cannot tag package sources"
+echo "p44mbrd-1.3.0.10: cannot push to http URL https://github.com/plan44/p44mbrd.git -> cannot tag package sources"
+echo "p44maintd-4.5.1: cannot push to http URL https://github.com/plan44/p44maintd.git -> cannot tag package sources"
+echo "mg44-3.14: cannot push to http URL https://github.com/plan44/mg44.git -> cannot tag package sources"
+echo "serialfwd-1.3: cannot push to http URL https://github.com/plan44/serialfwd.git -> cannot tag package sources"
+echo "omega2-ctrl-0.3: cannot push to http URL https://github.com/OnionIoT/omega2-ctrl.git -> cannot tag package sources"
+popd >/dev/null
